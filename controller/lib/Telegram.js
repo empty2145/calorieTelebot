c6ece173e1723df5cab961432151371711071614
empty2145 <tasybaevtamir@gmail.com>
2026-07-12 18:17:21 +0500

    setup

---
+++ controller/lib/Telegram.js
@@ -0,0 +1,30 @@
+const { axiosInstance} = require('./axios');
+
+function sendMessage(messageObj, messageText) {
+    return axiosInstance.get("sendMessage", {
+        chat_id: messageObj.chat.id,
+        text: messageText,
+    })
+}
+
+function handleMessage(messageObj) {
+    if (!messageObj.text) return;
+    const messageText = messageObj.text;
+
+    if (messageText.charAt(0) === "/") {
+        const command = messageText.substr(1);
+        switch (command) {
+            case "start":
+                return sendMessage(
+                    messageObj,
+                    "Hi! I am a bot. I can help you to get started"
+                );
+            default:
+                return sendMessage(messageObj, "Hey hi, I don't know that command")
+        }
+    } else {
+        return sendMessage(messageObj, messageText);
+    }
+}
+
+module.exports = { handleMessage };
---
+++ controller/lib/axios.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const MY_TOKEN = ""
+const BASE_URL = "https://api.telegram.org/bot" + MY_TOKEN;
+
+function getAxiosInstance() {
+    return {
+        get(method, params) {
+            return axios.get(`/${method}`, {
+                baseURL: BASE_URL,
+                params,
+            })
+        },
+        post(method, data) {
+            return axios.post({
+                method: "post",
+                baseURL: BASE_URL,
+                url: `/${method}`,
+                data
+            })
+        }
+    }
+}
+
+module.exports = { axiosInstance: getAxiosInstance() };
