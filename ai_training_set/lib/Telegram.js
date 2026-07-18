d32f196924c6cb4e4fd779972bb6affe689bfe12
empty2145 <tasybaevtamir@gmail.com>
2026-07-18 21:00:36 +0500

    train

---
+++ ai_training_set/lib/Telegram.js
@@ -0,0 +1,41 @@
+// lib/Telegram.js
+const { axiosInstance} = require('./axios');
+
+function sendMessage(messageObj, messageText) {
+    return axiosInstance.post("sendMessage", {
+        chat_id: messageObj.chat.id,
+        text: messageText,
+    })
+}
+
+function handleMessage(messageObj) {
+    if (!(messageObj.text || messageObj.photo)) return;
+    if (messageObj.photo) handlePhoto(messageObj);
+    if (messageObj.text) handleText(messageObj);
+}
+
+function handleText(messageObj) {
+    const messageText = messageObj.text;
+
+    if (messageText.charAt(0) === "/") {
+        const command = messageText.substr(1);
+        switch (command) {
+            case "start":
+                return sendMessage(
+                    messageObj,
+                    "Hi! I am a bot. I can help you to get started"
+                );
+            default:
+                return sendMessage(messageObj, "Hey hi, I don't know that command")
+        }
+    } else {
+        return sendMessage(messageObj, messageText);
+    }
+}
+
+
+function handlePhoto(messageObj) {
+    
+}
+
+module.exports = { handleMessage };
---
+++ ai_training_set/lib/axios.js
@@ -0,0 +1,20 @@
+// lib/axios.js
+const axios = require('axios');
+const MY_TOKEN = process.env.MY_TOKEN
+const BASE_URL = "https://api.telegram.org/bot" + MY_TOKEN;
+
+function getAxiosInstance() {
+    return {
+        get(method, params) {
+            return axios.get(`/${method}`, {
+                baseURL: BASE_URL,
+                params,
+            })
+        },
+        post(url, data, config) {
+            return axios.post(`/${url}`, data, {baseURL: BASE_URL })
+        }
+    }
+}
+
+module.exports = { axiosInstance: getAxiosInstance() };
