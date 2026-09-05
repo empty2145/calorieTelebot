dcb7103bdad14e73b7c07cb3bfc291cd1d8bfe4a
empty2145 <tasybaevtamir@gmail.com>
2026-09-05 14:40:02 +0500

    create: function lookup nutrition

--- controller/lib/axios.js
+++ controller/lib/axios.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const MY_TOKEN = process.env.MY_TOKEN
+const MY_TOKEN = process.env.MY_TOKEN;
 const BASE_URL = "https://api.telegram.org/bot" + MY_TOKEN;
 
 function getAxiosInstance() {
@@ -14,6 +14,19 @@ function getAxiosInstance() {
             return axios.post(`/${url}`, data, {baseURL: BASE_URL })
         }
     }
+}
+
+function lookupNutrition(foodName) {
+    const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search`, {
+        params: {
+            api_key: process.env.USDA_API_KEY,
+            query: foodName
+        }
+    });
+
+    const foods = response.data.foods;
+    const match = foods.find(food => food.dataType === "SR Legacy");
+
 }
 
 module.exports = { getAxiosInstance: getAxiosInstance };
