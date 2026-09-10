5f245c2dd2dcee05df1a286cc6290c530e700abd
empty2145 <tasybaevtamir@gmail.com>
2026-09-10 22:34:19 +0500

    fix: require

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const connectDB = require("./controller/db");
+const connectDB = require("./controller/lib/db");
 const express = require('express');
 const PORT = process.env.PORT || 4040;
 const { handler } = require("./controller/index");
