d37e8ada0524e463354be070ba1b4c021097e726
empty2145 <tasybaevtamir@gmail.com>
2026-09-10 13:18:13 +0500

    create: new file that connects to the db

---
+++ controller/lib/db.js
@@ -0,0 +1,14 @@
+// lib/db.js
+const mongoose = require('mongoose');
+
+const mealSchema = new mongoose,Schema({
+    userId: {type: Number, required: true },
+    timestamp: { type: Date, default: Date.now },
+    grandTotals: {
+        calories: Number,
+        protein: Number,
+        fat: Number,
+        carbs: Number
+    },
+    foods: Array
+});
